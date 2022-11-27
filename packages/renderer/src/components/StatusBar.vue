b85214a9ce861f7322f7551fb755d5dc2cf00085
Philippe Cohen <cohen.philippe@hotmail.com>
2022-11-27 12:29:31 +0100

    status bar and input menu button created - Not link component yet

---
+++ packages/renderer/src/components/StatusBar.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="accueil">
+        <p class="status">Accueil</p>
+    </div>
+</template>
+
+<style>
+
+
+.accueil {
+    width: auto;
+    height: 82px;
+    background: #3A4C8A;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+}
+
+.status {
+  font-family: inter-regular, sans-serif;
+  color: #FFFFFF;
+  font-size: 1.5em;
+  padding: 25px 40px;
+}
+</style>
